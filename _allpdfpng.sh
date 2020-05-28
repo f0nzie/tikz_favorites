1c0de126f850e6e401494186ec352010308816cb
fonzie zbook <alfonso.reyes@oilgainsanalytics.com>
2020-05-28 11:28:56 -0500

    scripts modified for Mac

---
+++ _allpdfpng.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# script to build PDF and PNG files executing my custom script `pdflatexpng`
+
+
+  cd src/    # change to where tex files are
+
+# remove auxiliary files
+  rm *.pdf *.png *.gz
+  rm *.aux *.log  *.nav *.out *.pnm
+
+# iterate through all .tex files (w/some exceptions). execute custom script pdflatexpng
+  find . -maxdepth 1  -name \*.tex -not -name "*.code.tex" -not -name "*.lualatex.tex"  -print0 | xargs -0 -I{} ../pdflatexpng {}
+  # xargs -0 -I{} ../pdflatexpng {}  run pdfltexpng for each .tex file
+  # pdflatexpng located in the project root folder
+  # TODO: build PDF and PNG for LuaLatex
+
+cd ..      # go back to project folder
