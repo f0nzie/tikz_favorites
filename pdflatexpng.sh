3bb3d09774c045ce08f43a136bc919f8de00ceb6
AlfonsoRReyes <alfonso.r.reyes@gmail.com>
2020-05-29 16:44:01 -0500

    added detection of operating system

--- pdflatexpng.sh
+++ pdflatexpng.sh
@@ -7,10 +7,32 @@ name=${file%.*}   # get the name of the file with no extension
 
 echo $file
 
-pdflatex -interaction=batchmode $file > /dev/null 2>&1 && gs -q -sDEVICE=png256 -sBATCH -sOutputFile=$name.png -dNOPAUSE -r1200 $name.pdf
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+# echo ${machine}
+
+
+if [ ${machine} = Linux ];then
+    echo Linux rulez
+    pdflatex -interaction=batchmode $file > /dev/null 2>&1
+    pdftoppm -q -png $name.pdf > $name.png # https://superuser.com/a/185897/653825
+elif [ ${machine} = Mac ];then    
+    echo Mac rulez
+    pdflatex -interaction=batchmode $file > /dev/null 2>&1 && gs -q -sDEVICE=png256 -sBATCH -sOutputFile=$name.png -dNOPAUSE -r1200 $name.pdf
+else 
+    echo unknown
+fi
+
 
 # ls -lh $name.png | cut -d " " -f14 -f10 # FIX sometimes doesn't return right column
 du -sh $name.png   # returns file size and name of PNG
 
+rm $name.pdf   # will not include PDF files
 rm $name.log
 rm $name.aux
