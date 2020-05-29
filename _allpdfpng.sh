b00c6da6ff95b381a5c3642420d54d762f3f9cdc
AlfonsoRReyes <alfonso.r.reyes@gmail.com>
2020-05-29 17:06:13 -0500

    add comments

--- _allpdfpng.sh
+++ _allpdfpng.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-# script to build multiple PDF and PNG files executing custom script `pdflatexpng`
+# script to build multiple PDF and PNG files executing custom script `pdflatexpng.sh`
 
 
-  cd src/    # change to where tex files are
+cd src/    # change to where tex files are
 
 # remove auxiliary files
   rm *.pdf *.png *.gz
   rm *.aux *.log  *.nav *.out *.pnm
 
 # iterate through all .tex files (w/some exceptions). execute custom script pdflatexpng
-  find . -maxdepth 1  -name \*.tex -not -name "*.code.tex" -not -name "*.lualatex.tex"  -print0 | xargs -0 -I{} ../pdflatexpng {}
+  find . -maxdepth 1  -name \*.tex -not -name "*.code.tex" -not -name "*.lualatex.tex"  -print0 | xargs -0 -I{} ../pdflatexpng.sh {}
+  # Explanation:
   # xargs -0 -I{} ../pdflatexpng {}  run pdfltexpng for each .tex file
   # pdflatexpng located in the project root folder
   # TODO: build PDF and PNG for LuaLatex
